d5cfdb81ce4882dc459f872eea7f3bd730febc4d
Victor14791012 <131807274+Victor14791012@users.noreply.github.com>
2023-06-07 14:20:26 -0400

    Update exer_1.js

--- exer_1.js
+++ exer_1.js
@@ -1,61 +1,67 @@
 /*
   Os atributos nomes, idade e especie são todos necessários. Assim, usando Try e Catch trate os erros e apresente uma mensagem de erro quando houve atributos vazios.
 */
-
-class Animal {
-  constructor(nome, idade, especie) {
-    this.nome = nome;
-    this.idade = idade;
-    this.especie = especie;
-  }
-
-
-
-  mostrarAtributos(){
-    try{ 
-      return this.atributos()
-      } catch(error){
-       console.log(error.message)
-    
-    
-      }
-
-   
-  } 
-
-  atributos() {
-    if(this.nome != "" && this.idade != "" && this.especie != "" ){
-    return {
-      nome: this.nome,
-      idade: this.idade,
-      especie: this.especie
+class MeuErro extends Error {
+    constructor(message){
+      super(message);
+      this.name = "Meu Erro";
     }
   }
 
-  else{
-    throw new Error ("Tem que colocar o nome , idade , espécie")
-}
-
+class Animal {
+    constructor(nome, idade, especie) {
+      this.nome = nome;
+      this.idade = idade;
+      this.especie = especie;
+    }
+  
+  
+  
+    mostrarAtributos(){
+      try{ 
+        return this.atributos()
+        } catch(error){
+         console.log(error.message)
+      
+      
+        }
+  
+     
+    } 
+  
+    atributos() {
+      if(this.nome != "" && this.idade != "" && this.especie != "" ){
+      return {
+        nome: this.nome,
+        idade: this.idade,
+        especie: this.especie
+      }
+    }
+  
+    else{
+        throw new MeuErro("Tem que colocar o nome , idade , espécie")
   }
-}
-
-const meuAnimal = new Animal("Fido", 3, "");
-const atributos = meuAnimal.mostrarAtributos();
-
-
- 
-
-
-
-
-
-
-
-// Imprimindo os atributos
-console.log(atributos.nome);   
-console.log(atributos.idade);  
-console.log(atributos.especie); 
-
-
-
-
+  
+    }
+  }
+  
+  const meuAnimal = new Animal("Fido", 3, "");
+  const atributos = meuAnimal.mostrarAtributos();
+  
+  
+   
+  
+  
+  
+  
+  
+  
+  
+  // Imprimindo os atributos
+  //console.log(atributos.nome);   
+  //console.log(atributos.idade);  
+  //console.log(atributos.especie); 
+  
+  
+  
+  
