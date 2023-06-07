b89167dd72dbf456afd5fe852a0e2f41e8d9d1b0
Victor14791012 <131807274+Victor14791012@users.noreply.github.com>
2023-06-07 14:30:13 -0400

    Update exer_3.js

--- exer_3.js
+++ exer_3.js
@@ -1,27 +1,46 @@
-class NerdIF {
-  constructor(estudante, cosplay, nota_cosplay) {
-    this.estudante = estudante;
-    this.cosplay = cosplay;
-    this.nota_cosplay = nota_cosplay;
+class MeuErro extends Error {
+    constructor(message){
+      super(message);
+      this.name = "Meu Erro";
+    }
   }
 
-  retornarAtributos() {
-    return this.atributos()
-  }
+  class NerdIF {
+    constructor(estudante, cosplay, nota_cosplay) {
+      this.estudante = estudante;
+      this.cosplay = cosplay;
+      this.nota_cosplay = nota_cosplay;
+    }
+  
+   mostrarAtributos() {
+      try {
+        return this.atributos();
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  
 
-  atributos() {
-    return {
-      estudante: this.estudante,
-      cosplay: this.cosplay,
-      nota_cosplay: this.nota_cosplay
-    };
-  }
-}
+    atributos() {
+        if (this.estudante != "" && this.cosplay != "" && this.nota_cosplay != "" ){
+          return {
+          estudante: this.estudante,
+          cosplay: this.cosplay,
+          nota_cosplay: this.nota_cosplay
+          };
+        } else {
+          throw new MeuErro("Mensagem de erro");
+        }
+      }
+    }
 
-const aluno = new NerdIF("João", "Homem-Aranha", 9.5);
-const atributos = aluno.retornarAtributos();
-
-console.log(atributos.estudante); 
-console.log(atributos.cosplay);   
-console.log(atributos.nota_cosplay);
-
+   
+  
+  const aluno = new NerdIF("", "Homem-Aranha", 9.5);
+  const atributos = aluno.mostrarAtributos();
+  
+  console.log(atributos.estudante); 
+  console.log(atributos.cosplay);   
+  console.log(atributos.nota_cosplay);
+  
+  
